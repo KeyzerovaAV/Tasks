1d2567719b3966ee3a4e72f9d7f22483db7cdc92
Anastasia Keyzerova <key-nastia@mail.ru>
2023-01-31 23:48:07 +0300

    Added task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 2. Написать программу, которая на вход принимает два числа и выдает, 
+какое число больше.
+*/
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+if (number1 > number2)
+{
+    Console.WriteLine($"Больше число {number1}");
+}
+else
+{
+    Console.WriteLine($"Больше число {number2}");
+}
