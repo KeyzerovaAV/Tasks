ac87025077ee0df1f6d5bbae0419d8cea57340e8
Anastasia Keyzerova <key-nastia@mail.ru>
2023-01-31 23:41:47 +0300

    Added task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 1. Написать программу, которая на вход принимает два числа и проверяет, 
+является ли первое число квадратом второго.
+*/
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+if (number1 == number2 * number2)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
