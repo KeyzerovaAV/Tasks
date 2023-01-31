a8cfd513774c63c710a3a57032b70bc77193a071
Anastasia Keyzerova <key-nastia@mail.ru>
2023-01-31 23:35:45 +0300

    Added task 0

---
+++ Task0/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Задача 0. Написать программу, которая на вход принимает число и выдает его квадрат.
+*/
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int square = number * number;
+
+Console.WriteLine($"Квадрат числа {number} равен {square}");
