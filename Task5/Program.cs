10ec54f028b4f086882eff8cd268c2a56353d64c
Anastasia Keyzerova <key-nastia@mail.ru>
2023-02-05 15:00:09 +0300

    Added task 5

---
+++ Task5/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 5. Написать программу, которая на вход принимает одно число (N), 
+а на выходе показывает все целые числа в промежутке от -N до N.
+*/
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int number = -N;
+while (number <= N)
+{
+    Console.Write($"{number} ");
+    number = number + 1;
+}
