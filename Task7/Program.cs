b29cc2b43714d29d6dacaf1144fe774d78486d71
Anastasia Keyzerova <key-nastia@mail.ru>
2023-02-05 15:12:32 +0300

    Added task 7

---
+++ Task7/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 7. Написать программу, которая принимает на вход трехзначное число 
+и на выходе показывает последнюю цифру этого числа.
+*/
+
+Console.Clear();
+
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number < 0)
+{
+    number = number * -1;
+}
+
+Console.WriteLine(number % 10);
