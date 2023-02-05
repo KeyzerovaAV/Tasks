00b71e97a12961d9bcea3a89e3c2c65081a0aee1
Anastasia Keyzerova <key-nastia@mail.ru>
2023-02-05 15:49:35 +0300

    Added task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 10. Написать программу, которая принимает на вход трехзначное число 
+и на выходе показывает вторую цифру этого числа.
+*/
+
+Console.Clear();
+
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number < 100 || number > 999)
+{
+    Console.WriteLine("ошибка");
+}
+
+else
+{
+    int getSecondDigit(int number)
+    {
+        int secondDigit = number / 10 % 10;
+        return secondDigit;
+    }
+
+    int secondDigit = getSecondDigit(number);
+    Console.WriteLine($"Вторая цифра числа {number} - это {secondDigit}");
+}
