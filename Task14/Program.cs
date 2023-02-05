320010aab67558f020c340e2faf87083b7f564b6
Anastasia Keyzerova <key-nastia@mail.ru>
2023-02-05 16:44:57 +0300

    Added task 14

---
+++ Task14/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 14. Написать программу, которая принимает на вход число 
+и проверяет, кратно ли оно одновременно 7 и 23.
+*/
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int remain1 = number % 7;
+int remain2 = number % 23;
+
+if (remain1 == 0 && remain2 == 0)
+{
+    Console.WriteLine("да, кратно");
+}
+else
+{
+    Console.WriteLine($"нет, не кратно");
+}
