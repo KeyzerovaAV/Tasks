5d258b83864483ced2667f29de6ccfea73fc602b
Anastasia Keyzerova <key-nastia@mail.ru>
2023-02-05 16:00:17 +0300

    Added task 11

---
+++ Task11/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 11. Написать программу, которая выводит трехзначное число 
+и удаляет вторую цифру этого числа.
+*/
+
+Console.Clear();
+
+int getNumberFromRange(int start, int end)
+{
+    int number = new Random().Next(start, end + 1);
+    return number;
+}
+
+int deleteSecondDigit(int number)
+{
+    int digit1 = number / 100;
+    int digit3 = number % 10;
+    int newNumber = digit1 * 10 + digit3;
+    return newNumber;
+}
+
+int randomNumber = getNumberFromRange(100, 999);
+int newNumber = deleteSecondDigit(randomNumber);
+
+Console.WriteLine(randomNumber);
+Console.WriteLine($"Число {randomNumber} без второй цифры - это {newNumber}");    
