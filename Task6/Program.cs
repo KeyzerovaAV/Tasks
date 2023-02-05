9593f63028d00384825eb5c4cb2bfa335c39dcba
Anastasia Keyzerova <key-nastia@mail.ru>
2023-02-05 15:06:04 +0300

    Added task 6

---
+++ Task6/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 6. Написать программу, которая на вход принимает число 
+и выдает, является ли оно четным.
+*/
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
