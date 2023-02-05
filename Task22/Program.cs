ddf97af5b1311eb570737ae549c86068bbc0cdfd
Anastasia Keyzerova <key-nastia@mail.ru>
2023-02-05 18:57:31 +0300

    Added task 22

---
+++ Task22/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 22. Написать программу, которая принимает на вход число (N) 
+и выдает таблицу квадратов от 1 до N.
+*/
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+for (int number = 1; number <= N; number++)
+{
+    if (number < N)
+    {
+        Console.Write($"{Math.Pow(number, 2)}, ");
+    }
+    else
+    {
+        Console.Write($"{Math.Pow(number, 2)} ");
+    }
+}
