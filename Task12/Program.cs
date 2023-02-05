38167870dca22d99f0fe25339542037424a758f7
Anastasia Keyzerova <key-nastia@mail.ru>
2023-02-05 16:12:08 +0300

    Added task 12

---
+++ Task12/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 12. Написать программу, которая принимает на вход два числа и выводит, 
+является ли второе число кратным первому, а если не является, выводит остаток от деления.
+*/
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+int getRemain(int number1, int number2)
+{
+    int remain = number1 % number2;
+    return remain;
+}
+
+int remain = getRemain(number1, number2);
+
+if (remain == 0)
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+    Console.WriteLine($"не кратно, остаток от деления = {remain}");
+}
