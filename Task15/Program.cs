a740f83631686c849d7093a05b570fc3be60e8a8
Anastasia Keyzerova <key-nastia@mail.ru>
2023-02-05 16:53:25 +0300

    Added task 15

---
+++ Task15/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 15. Написать программу, которая принимает на вход цифру, обозначающую день недели, 
+и проверяет, является ли этот день выходным.
+*/
+
+Console.Clear();
+
+Console.Write("Введите цифру от 1 до 7: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number <= 7)
+{
+    if (number <= 5)
+    {
+        Console.WriteLine("нет, будний день");
+    }
+    else
+    {
+        Console.WriteLine("да, выходной день");
+    }
+}
+else
+{
+    Console.WriteLine("ошибка");
+}
