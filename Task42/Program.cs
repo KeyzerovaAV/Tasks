7b77bf4be7e6477f1512bf44571dae93baf797bb
Anastasia Keyzerova <key-nastia@mail.ru>
2023-02-06 02:27:03 +0300

    Added task 42

---
+++ Task42/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 42. Написать программу, которая преобразует десятичное число в двоичное.
+*/
+
+Console.Clear();
+
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.Write(message);
+    Console.ResetColor();
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+string convertToBinaryNumber(int number)
+{
+    string result = string.Empty;
+    while (number > 0)
+    {
+        result = number % 2 + result;
+        number = number / 2;
+    }
+    return result;    
+}
+
+int number = getUserData("Введите число: ");
+string binaryNumber = convertToBinaryNumber(number);
+
+Console.WriteLine($"Число {number} в двоичной системе = {binaryNumber}");
