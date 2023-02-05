efcd004bf88d2781644dce5ba9dff6bfbf69949c
Anastasia Keyzerova <key-nastia@mail.ru>
2023-02-05 19:01:57 +0300

    Added task 23

---
+++ Task23/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 23. Написать программу, которая принимает на вход число (N) 
+и выдает таблицу кубов от 1 до N.
+*/
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+for (int number = 1; number <= N; number++)
+{
+    if (number < N)
+    {
+        Console.Write($"{Math.Pow(number, 3)}, ");
+    }
+    else
+    {
+        Console.Write($"{Math.Pow(number, 3)} ");
+    }
+}
