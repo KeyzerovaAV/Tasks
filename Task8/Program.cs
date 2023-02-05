1cfedb32ba375896a8adc5590489d4cc86c58dcb
Anastasia Keyzerova <key-nastia@mail.ru>
2023-02-05 15:17:20 +0300

    Added task 8

---
+++ Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 8. Написать программу, которая на вход принимает число (N), 
+а на выходе показывает все четные числа от 1 до N.
+*/
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int number = 2;
+while (number <= N)
+{
+    Console.Write($"{number} ");
+    number = number + 2;
+}
