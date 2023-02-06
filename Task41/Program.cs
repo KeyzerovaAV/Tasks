82835d85c1729253750e17aecf1c1089f2c19e74
Anastasia Keyzerova <key-nastia@mail.ru>
2023-02-07 01:46:31 +0300

    Added task 41

---
+++ Task41/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Задача 41. Пользователь вводит с клавиатуры M чисел. 
+Посчитать, сколько чисел больше 0 ввел пользователь.
+*/
+
+Console.Clear();
+
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.Write(message);
+    Console.ResetColor();
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+int countPositivesInRangeOfUserNumbers(int M)
+{
+    int count = 0;
+    string range = string.Empty;
+    for (int i = 0; i < M; i++)
+    {
+        int number = getUserData("Введите число: ");
+        if (number > 0)
+        {
+            count++;
+        }
+        if (i < M - 1)
+        {
+            range = ($"{range}{number}, ");
+        }
+        else
+        {
+            range = ($"{range}{number}");
+        }
+    }
+    Console.WriteLine(range);
+    return count;
+}
+
+int M = getUserData("Введите количество чисел: ");
+int count = countPositivesInRangeOfUserNumbers(M);
+
+Console.WriteLine($"Количество чисел больше нуля = {count}");
