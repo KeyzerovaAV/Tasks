147b427f813f72bf3ae4716e075d200be242069e
Anastasia Keyzerova <key-nastia@mail.ru>
2023-02-07 03:17:37 +0300

    Added task 43

---
+++ Task43/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Задача 43. Написать программу, которая находит точку пересечения двух прямых, 
+заданных уравнениями: y = k1 * x + b1, y = k2 * x + b2. 
+Значения b1, k1, b2 и k2 задаются пользователем.
+*/
+
+Console.Clear();
+
+int getUserData(string message)
+{
+    Console.Write(message);
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+double b1 = getUserData("Введите b1: ");
+double k1 = getUserData("Введите k1: ");
+double b2 = getUserData("Введите b2: ");
+double k2 = getUserData("Введите k2: ");
+
+if (k1 == k2)
+{
+    Console.WriteLine("Прямые параллельны друг другу");
+}
+else
+{
+    double findX(double b1, double k1, double b2, double k2)
+    {
+        double x = Math.Round(((b2 - b1) / (k1 - k2)), 2);
+        return x;
+    }
+
+    double findY(double b1, double k1, double x)
+    {
+        double y = Math.Round((k1 * x + b1), 2);
+        return y;
+    }
+
+
+    double x = findX(b1, k1, b2, k2);
+    double y = findY(b1, k1, x);
+
+    Console.WriteLine($"Точка пересечения прямых: ({x}; {y})");
+}
