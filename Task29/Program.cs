6a572d3abb21232910224c57e5ad76f18233de8f
Anastasia Keyzerova <key-nastia@mail.ru>
2023-02-10 02:26:47 +0300

    Added task 29

---
+++ Task29/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Задача 29. Написать программу, которая задает массив из 8 элементов 
+и выводит их на экран.
+*/
+
+Console.Clear();
+
+int getRandom()
+{
+    return new Random().Next(0, 100);
+}
+
+int[] generateArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = getRandom();
+    }
+    return array;
+}
+
+string getStringArray(int[] array)
+{
+    string stringArray = "[";
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1)
+        {
+            stringArray = stringArray + $"{array[i]},";
+        }
+        else
+        {
+            stringArray = stringArray + $"{array[i]}]";
+        }
+    }
+    return stringArray;
+}
+
+int[] array = generateArray(8);
+string stringArray = getStringArray(array);
+
+Console.Write(stringArray);
