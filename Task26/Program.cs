19ab3efc7c9a968aee574487bbe24ef3e065811e
Anastasia Keyzerova <key-nastia@mail.ru>
2023-02-10 01:34:36 +0300

    Added task 26

---
+++ Task26/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Задача 26. Написать программу, которая принимает на вход число 
+и выдает количество цифр в числе.
+*/
+
+Console.Clear();
+
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write(message);
+    Console.ResetColor();
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+int getNumberOfDigits(int number)
+{
+    int numberOfDigits = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        numberOfDigits++;
+    }
+    return numberOfDigits;
+}
+
+void showData(string messageStart, int data)
+{
+    Console.Write(messageStart);
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+int number = getUserData("Введите число больше 0: ");
+int numberOfDigits = getNumberOfDigits(number);
+showData($"В числе {number} количество цифр = ", numberOfDigits);
