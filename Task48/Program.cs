be6923d78450267e493146a1362fa0d50adc94eb
Anastasia Keyzerova <key-nastia@mail.ru>
2023-02-11 23:12:51 +0300

    Added task 48

---
+++ Task48/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+Задача 48. Задать двумерный массив размера m на n, 
+каждый элемент которого находится по формуле: Aₘₙ = m+n. 
+Вывести полученный массив на экран.
+*/
+
+Console.Clear();
+
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.Write(message);
+    Console.ResetColor();
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+void printInColor(string data)
+{
+    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+int[,] get2DArray(int colLength, int rowLength)
+{
+    int[,] array = new int[colLength, rowLength];
+    for (int i = 0; i < colLength; i++)
+    {
+        for (int j = 0; j < rowLength; j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+void print2DArray(int[,] array)
+{
+    Console.Write("\t");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        printInColor(j + "\t");
+    }
+    Console.WriteLine();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printInColor(i + "\t");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = getUserData("Введите количество строк: ");
+int n = getUserData("Введите количество столбцов: ");
+int[,] array = get2DArray(m, n);
+print2DArray(array);
