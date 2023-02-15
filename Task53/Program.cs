7371d327f29c37251dca91b97a7f00c2f6135e44
Anastasia Keyzerova <key-nastia@mail.ru>
2023-02-15 23:13:50 +0300

    Аdded task 53

---
+++ Task53/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+Задача 53. Задать двумерный массив. Написать программу, 
+которая поменяет местами первую и последнюю строку массива.
+*/
+
+Console.Clear();
+
+int[,] generate2DArray(int rowLength, int colLength, int start, int end)
+{
+    int[,] array = new int[rowLength, colLength];
+    for (int i = 0; i < rowLength; i++)
+    {
+        for (int j = 0; j < colLength; j++)
+        {
+            array[i, j] = new Random().Next(start, end + 1);
+        }
+    }
+    return array;
+}
+
+void printInColor(string data, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+void printHeadOfArray(int length)
+{
+    Console.Write("\t");
+    for (int i = 0; i < length; i++)
+    {
+        printInColor(i + "\t", ConsoleColor.DarkGreen);
+    }
+    Console.WriteLine();
+}
+
+void print2DArray(int[,] array)
+{
+    printHeadOfArray(array.GetLength(1));
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printInColor(i + "|\t", ConsoleColor.DarkMagenta);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine("-----------------------------------------------------------------------------");
+}
+
+int[,] swapRows(int[,] array, int row1, int row2)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        int temp = array[row1, j];
+        array[row1, j] = array[row2, j];
+        array[row2, j] = temp;
+    }
+    return array;
+}
+
+int[,] array = generate2DArray(4, 9, -10, 10);
+print2DArray(array);
+int[,] swappedArray = swapRows(array, 0, array.GetLength(0) - 1);
+print2DArray(swappedArray);
