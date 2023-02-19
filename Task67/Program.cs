156ec9b43a27d5f3b6ff9f113209fd30af761736
Anastasia Keyzerova <key-nastia@mail.ru>
2023-02-20 01:55:58 +0300

    Added task 67

---
+++ Task67/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 67. Написать программу, которая принимает на вход число 
+и возвращает сумму его цифр.
+*/
+
+Console.Clear();
+
+int getUserData(string message)
+{
+    printInColor(message, ConsoleColor.DarkGreen);
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+void printInColor(string data, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+int getSumOfDigits(int userNumber)
+{
+    if (userNumber == 0)
+    {
+        return 0;
+    }
+    return userNumber % 10 + getSumOfDigits(userNumber / 10);
+}
+
+int userNumber = getUserData("Введите число больше 0: ");
+int sumOfDigits = getSumOfDigits(userNumber);
+
+Console.Write($"Сумма цифр числа {userNumber} = ");
+printInColor($"{sumOfDigits}", ConsoleColor.DarkMagenta);
