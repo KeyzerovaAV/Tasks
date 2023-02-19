89294a020ab556c7e7d13b3fb31b9cf60f146aa9
Anastasia Keyzerova <key-nastia@mail.ru>
2023-02-20 00:38:20 +0300

    Added task 63

---
+++ Task63/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 63. Задать значение N. Написать программу, 
+которая выводит все натуральные числа в промежутке от 1 до N.
+*/
+
+Console.Clear();
+
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.Write(message);
+    Console.ResetColor();
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+int userNumber = getUserData("Введите значение - границу ряда: ");
+
+void printRange(int N)
+{
+    if (N == 0)
+    {
+        return;
+    }
+    printRange(N - 1);
+    if (N == userNumber) Console.Write(N);
+    else Console.Write(N + ", ");
+}
+
+printRange(userNumber);
