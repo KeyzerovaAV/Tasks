062df86e2947622f7d18703b809095e82b8682b2
Anastasia Keyzerova <key-nastia@mail.ru>
2023-02-24 17:37:26 +0300

    Changed task 19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -8,23 +8,27 @@ Console.Clear();
 Console.Write("Введите пятизначное число: ");
 int number = int.Parse(Console.ReadLine()!);
 
-int digit1 = number / 10000;
-int digit2 = number / 1000 % 10;
-int digit4 = number / 10 % 10;
-int digit5 = number % 10;
-
-if (number > 1000 && number <= 99999)
+void checkIfPalindrome(int number)
 {
-    if (digit1 == digit5 && digit2 == digit4)
+    int digit1 = number / 10000;
+    int digit2 = number / 1000 % 10;
+    int digit4 = number / 10 % 10;
+    int digit5 = number % 10;
+    if (number > 1000 && number <= 99999)
     {
-        Console.WriteLine($"Число {number} является палиндромом");
+        if (digit1 == digit5 && digit2 == digit4)
+        {
+            Console.WriteLine($"Число {number} является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine($"Число {number} не является палиндромом");
+        }
     }
     else
     {
-        Console.WriteLine($"Число {number} не является палиндромом");
+        Console.WriteLine("ошибка");
     }
 }
-else
-{
-    Console.WriteLine("ошибка");
-}
+
+checkIfPalindrome(number);
